2078f3c0762553068d9a21809ae2efdfe6fada87
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-06-01 09:08:18 +0100

    UserController

---
+++ controller/UserController.js
@@ -0,0 +1,24 @@
+import dbClient from '../utils/db';
+import sha1 from 'sha1';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+    } else if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+    } else {
+      const user = await dbClient.searchUser(email);
+      if (user) res.status(400).json({ error: 'Already exist' });
+      else {
+        const passwd = sha1(passwd);
+        const user = await dbClient.saveUser(email, passwd);
+        res.status(201).json({ id: user.insertedId, email: user.ops[0].email });
+      }
+    }
+  }
+}
+
+export default UsersController;
