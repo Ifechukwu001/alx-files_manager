58cf9dd1bebecf115fa479e170c15e2f406cf0dc
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-06-01 09:02:32 +0100

    Redis file

---
+++ utils/redis.js
@@ -0,0 +1,40 @@
+// Redis utils
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log(err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getValue = promisify(this.client.get).bind(this.client);
+    try {
+      return await getValue(key);
+    } catch (err) {
+      return (err.message);
+    }
+  }
+
+  async set(key, value, duration) {
+    await this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    await this.client.del(key, (err, reply) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Deleted ${reply} keys`);
+      }
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
