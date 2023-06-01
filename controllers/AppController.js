25e7b1b5af8f582cb738b36dfc73b4bf99329dd7
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-06-01 09:18:09 +0100

    Appcontroller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,16 @@
-// controller routes
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  // appcontroller class
-  static getStatus(req, res) {
-    const redisLive = redisClient.isAlive();
-    const dbLive = dbClient.isAlive();
-    res.status(200).json({ redis: redisLive, db: dbLive });
+  static getStatus(request, response) {
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  static async getStats(req, res) {
-    const userCount = dbClient.nbUsers();
-    const fileCount = dbClient.nbFiles();
-    res.status(200).json({ users: userCount, files: fileCount });
+  static async getStats(request, response) {
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
   }
 }
 
-export default AppController;
+module.exports = AppController;
