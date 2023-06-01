8e2e5a9742e8aa63b7cac4324984dbcf35e5b4c7
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-06-01 09:03:40 +0100

    DB file

---
+++ utils/db.js
@@ -0,0 +1,48 @@
+// Database client class
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const db = process.env.DV_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${db}`;
+    this.client = new MongoClient(uri);
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    this.db = this.client.db();
+    this.users = this.db.collection('users');
+    const userNum = await this.users.countDocuments();
+    return userNum;
+  }
+
+  async nbFiles() {
+    this.db = this.client.db();
+    this.files = this.db.collection('files');
+    const fileNum = await this.files.countDocuments();
+    return fileNum;
+  }
+
+  async saveUser(email, passwd) {
+    this.db = this.client.db();
+    this.users = this.db.collection('users');
+    const user = await this.users.insert(email, passwd);
+    return user;
+  }
+
+  async searchUser(email) {
+    this.db = this.client.db();
+    this.users = this.db.collection('users');
+    const user = await this.users.find(email);
+    return user;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
